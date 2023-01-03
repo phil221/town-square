dd848c2d9bc05428337f98d6cab7b7df318580fa
Phil Meckley <phil@immaculatastudios.com>
2023-01-03 12:23:34 -0500

    wip like option w thubs up selected toggle

--- client/src/components/FeedItem.jsx
+++ client/src/components/FeedItem.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import convertDate from '../helpers/convertDate';
+import thumbsup from "../assets/fa-icons/thumbsup.svg";
+import thumbsupChecked from "../assets/fa-icons/thumbsupChecked.svg"
 
 function FeedItem({ post }) {
+  const [isChecked, setIsChecked] = useState(false);
+
+  function toggleThumbsUp(){
+    setIsChecked(!isChecked);
+  }
 
   return (
     <Card className="my-4">
@@ -10,6 +18,7 @@ function FeedItem({ post }) {
           <p>{post.content}</p>
           <footer className="blockquote-footer mt-0 d-flex justify-content-between">
             <span>@{post.username}</span>
+            <img onClick={toggleThumbsUp} src={isChecked ? thumbsupChecked : thumbsup} alt="thumbs up" />
             {post.published && convertDate(post.published)}
           </footer>
         </blockquote>
